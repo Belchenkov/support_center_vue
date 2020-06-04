b92eec0b210150eeba843ebb5d43099e22f2ec56
Aleksey Belchenkov <u608110@gmail.com>
2020-06-04 13:36:47 +0300

    Creating routes

--- src/router.js
+++ src/router.js
@@ -2,3 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
+
+import Home from './components/Home.vue';
+import FAQ from './components/FAQ.vue';
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/faq', name: 'faq', component: FAQ },
+];
