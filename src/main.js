b4fdebbb2d1a3f610a619db17a0b6e04568e04f3
Aleksey Belchenkov <u608110@gmail.com>
2020-06-10 14:02:15 +0300

    Write Smart form

--- src/main.js
+++ src/main.js
@@ -2,15 +2,23 @@ import Vue from 'vue'
 import 'babel-polyfill';
 
 import router from './router';
-import AppLayout from './components/AppLayout.vue';
+import state from './state';
 import './global-components';
+
+// Components
+import AppLayout from './components/AppLayout.vue';
+
+// Plugins
 import VueFetch from './plugins/fetch';
+import VueState from './plugins/state';
 Vue.use(VueFetch, {
   baseUrl: 'http://localhost:3000/',
 });
+Vue.use(VueState, state);
 
 new Vue({
   el: '#app',
+  data: state,
   render: h => h(AppLayout),
   router,
 })
